b6751671e5316f2fba977c75830e248d677558da
supahstarclod <621383@gmail.com>
2019-04-20 21:39:06 -0700

    tic-tac-toe v3

--- main-log.py
+++ main-log.py
@@ -22,8 +22,8 @@ async def on_message(message):
     board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     show = ""
     blank = ":white_small_square:"
-    os = ":stuck_out_tongue_winking_eye:"
-    xs = ":pensive:"
+    os = ":o:"
+    xs = ":x:"
     turncount = 1
 
     def move(num, turn):
